34fcf1c82067fff72b8304d3f9e871996b24db40
Matheus Lemes <matheus311095@yahoo.com.br>
2023-03-31 22:22:20 -0300

    Creates sideBar component and adds Phosphor Icons

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=30"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/70829665?s=400&u=88a845a74b852726b815b599675db8b5efab15a3&v=4"
+        />
+
+        <strong>Matheus Furlan</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} /> Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+};
