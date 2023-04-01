6c644f5728919df944a3fc564e9df432b45dddd9
Matheus Lemes <matheus311095@yahoo.com.br>
2023-03-31 22:59:28 -0300

    Updates App component and Global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+import "./global.css";
 
 export function App() {
   return (
     <div>
-      <Post author="Matheus Furlan" content="Publication content"></Post>
-      <Post author="Kauan" content="Publication content do Kauan"></Post>
-      <Post author="Alana" content="Publication content da Alana"></Post>
-      <Post
-        author="Diego Martins"
-        content="Publication content do Diego"
-      ></Post>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Matheus Furlan"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, facilis incidunt! Blanditiis doloribus facilis aspernatur quibusdam molestias tempora, ex suscipit, accusamus nemo, ullam alias! Temporibus non itaque vel consequuntur quis!"
+          ></Post>
+          <Post author="Kauan" content="Publication content do Kauan"></Post>
+        </main>
+      </div>
     </div>
   );
 }
